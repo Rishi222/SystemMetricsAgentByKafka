60cefd601f284e5750d28f9e948eb78462a2e6de
Rishi Sharma <rishisharma222002@gmail.com>
2025-09-28 19:31:54 +0530

    modify kafka.js with some additional parameters

--- config/kafka.js
+++ config/kafka.js
@@ -4,6 +4,8 @@ const { Kafka } = require("kafkajs");
 const kafka = new Kafka({
   clientId: process.env.CLIENT_ID_CONSUMER || "system-monitor-consumer",
   brokers: (process.env.BROKERS || "localhost:9092").split(","),
+  connectionTimeout: 3000,
+  requestTimeout: 25000,
 });
 
 module.exports = kafka;
